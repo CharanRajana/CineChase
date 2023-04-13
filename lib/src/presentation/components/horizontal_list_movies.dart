a1071d085252451403b8dad1b002984a4d243c30
Charan Rajana <saicharanrajana@gmail.com>
2023-04-14 00:05:11 +0530

    refactored folder structure

---
+++ lib/src/presentation/components/horizontal_list_movies.dart
@@ -0,0 +1,96 @@
+import 'package:flutter/material.dart';
+import 'package:go_router/go_router.dart';
+import '../../core/extras/assets.dart';
+import '../../core/extras/constants.dart';
+import '../../data/movies_api_client/models/movies_model.dart';
+
+class MoviesList extends StatelessWidget {
+  const MoviesList({
+    super.key,
+    required this.title,
+    required this.movies,
+  });
+
+  final String title;
+  final List<Movie> movies;
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      padding: const EdgeInsets.all(10.0),
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          Padding(
+            padding: const EdgeInsets.symmetric(vertical: 8, horizontal: 10),
+            child: Text(
+              title,
+              style: Theme.of(context).textTheme.headlineSmall?.copyWith(
+                    fontWeight: FontWeight.bold,
+                    fontSize: 20,
+                  ),
+            ),
+          ),
+          SizedBox(
+            height: 300,
+            child: ListView.separated(
+              scrollDirection: Axis.horizontal,
+              itemBuilder: (context, index) {
+                return SizedBox(
+                  width: 250,
+                  child: GestureDetector(
+                    onTap: () => context.goNamed('movies',
+                        params: {'id': '${movies[index].id}'}),
+                    child: Card(
+                      clipBehavior: Clip.antiAlias,
+                      child: Padding(
+                        padding: const EdgeInsets.all(8.0),
+                        child: GridTile(
+                          footer: GridTileBar(
+                            backgroundColor: Colors.black54,
+                            title: Text(
+                              movies[index].title,
+                              textAlign: TextAlign.center,
+                              style: Theme.of(context)
+                                  .textTheme
+                                  .titleMedium
+                                  ?.copyWith(
+                                    fontWeight: FontWeight.bold,
+                                    color: Colors.amber,
+                                  ),
+                            ),
+                          ),
+                          child: FadeInImage(
+                            placeholder: const AssetImage(Assets.placeholder),
+                            image: movies[index].posterPath == ''
+                                ? const AssetImage(
+                                    Assets.placeholder,
+                                  ) as ImageProvider
+                                : NetworkImage(
+                                    Constants.posterPrefix +
+                                        movies[index].posterPath,
+                                  ),
+                            fit: BoxFit.cover,
+                          ),
+                        ),
+                      ),
+                    ),
+                  ),
+                );
+              },
+              separatorBuilder: (context, index) {
+                return const SizedBox(
+                  width: 10,
+                );
+              },
+              itemCount: movies.length,
+            ),
+          ),
+          const SizedBox(
+            height: 10,
+          )
+        ],
+      ),
+    );
+  }
+}
