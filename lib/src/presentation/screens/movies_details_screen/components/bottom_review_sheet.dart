a3f4a58619088d890876324577cf03ac7599d447
Charan Rajana <saicharanrajana@gmail.com>
2023-04-14 15:18:03 +0530

    changed

--- lib/src/presentation/screens/movies_details_screen/components/bottom_review_sheet.dart
+++ lib/src/presentation/screens/movies_details_screen/components/bottom_review_sheet.dart
@@ -7,34 +7,37 @@ import 'interactive_rating_bar.dart';
 Future<dynamic> bottomReviewSheet(
     BuildContext context, double ratings, WidgetRef ref) {
   return showModalBottomSheet(
-    constraints: BoxConstraints.tight(const Size(600, 200)),
+    constraints: BoxConstraints.tight(const Size(600, 180)),
     context: context,
     builder: (context) {
-      return Column(
-        mainAxisSize: MainAxisSize.max,
-        children: [
-          Padding(
-            padding: const EdgeInsets.symmetric(horizontal: 8, vertical: 16),
-            child: Text(
+      return Padding(
+        padding: const EdgeInsets.symmetric(horizontal: 42, vertical: 16),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+          crossAxisAlignment: CrossAxisAlignment.stretch,
+          children: [
+            Text(
               'Rate the Movie, Out of 5',
-              style: Theme.of(context).textTheme.headlineSmall,
+              style: Theme.of(context).textTheme.headlineMedium,
             ),
-          ),
-          InteractiveRatingBar(ratings: ratings),
-          Padding(
-            padding: const EdgeInsets.symmetric(horizontal: 8, vertical: 16),
-            child: GestureDetector(
-              onTap: () {
-                ref.read(ratingProvider.notifier).state = 0;
-              },
-              child: Text(
-                'clear',
-                textAlign: TextAlign.right,
-                style: Theme.of(context).textTheme.labelLarge,
+            InteractiveRatingBar(ratings: ratings),
+            Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 8, vertical: 16),
+              child: GestureDetector(
+                onTap: () {
+                  ref.read(ratingProvider.notifier).state = 0;
+                },
+                child: Align(
+                  alignment: Alignment.centerRight,
+                  child: Text(
+                    'clear',
+                    style: Theme.of(context).textTheme.bodyLarge,
+                  ),
+                ),
               ),
             ),
-          ),
-        ],
+          ],
+        ),
       );
     },
   );
