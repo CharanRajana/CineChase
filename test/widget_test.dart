75b03b7104bc6cabcefb7ef55a9b78e784f9604b
Charan Rajana <saicharanrajana@gmail.com>
2023-04-15 13:08:41 +0530

    renamed project title

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -8,7 +8,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 
-import 'package:project32/main.dart';
+import 'package:cinechase/main.dart';
 
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
