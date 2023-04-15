5d027720e25d1eaac4b75755864cc43d3c7a2a92
Charan Rajana <saicharanrajana@gmail.com>
2023-04-15 23:26:19 +0530

    altered the size of the icons

--- lib/src/presentation/screens/movies_details_screen/components/bottom_review_sheet.dart
+++ lib/src/presentation/screens/movies_details_screen/components/bottom_review_sheet.dart
@@ -1,7 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 
-import '../../../../core/extras/providers.dart';
 import 'interactive_rating_bar.dart';
 
 Future<dynamic> bottomReviewSheet(
@@ -14,28 +13,12 @@ Future<dynamic> bottomReviewSheet(
         padding: const EdgeInsets.symmetric(horizontal: 42, vertical: 16),
         child: Column(
           mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-          crossAxisAlignment: CrossAxisAlignment.stretch,
           children: [
             Text(
               'Rate the Movie, Out of 5',
               style: Theme.of(context).textTheme.headlineMedium,
             ),
             InteractiveRatingBar(ratings: ratings),
-            Padding(
-              padding: const EdgeInsets.symmetric(horizontal: 8, vertical: 16),
-              child: GestureDetector(
-                onTap: () {
-                  ref.read(ratingProvider.notifier).state = 0;
-                },
-                child: Align(
-                  alignment: Alignment.centerRight,
-                  child: Text(
-                    'clear',
-                    style: Theme.of(context).textTheme.bodyLarge,
-                  ),
-                ),
-              ),
-            ),
           ],
         ),
       );
